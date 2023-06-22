e68440f8cbb9e07bf00b1dea336c03e08bb2d3fe
sctop <2094880085@qq.com>
2023-06-23 04:06:02 +0800

    Fixed for multiple story descriptions, 2023/06/23

--- data_model/actual_data/story.py
+++ data_model/actual_data/story.py
@@ -3,6 +3,7 @@ from data_model.types.metatype.complex import *
 from data_model.actual_data._story.story_pos import *
 from data_model.loader import i18n_translator, constant_manager
 from data_model.actual_data.track import TrackInfo, TrackListManager
+from data_model.types.lang_string import MultipleLangStringModelList
 
 
 class StoryInfo(ToJsonReminderMixin):
@@ -19,10 +20,19 @@ class StoryInfo(ToJsonReminderMixin):
         self.is_battle = data["is_battle"]
 
         self.name = i18n_translator[data["name"]]
-        self.desc = i18n_translator[data["desc"]]
         self.pos = storyPosAuto(data["pos"])
         self.track = TrackListManager('track')
         self.track.load(data["track"])
+
+        # Special case: some stories still have content after the battle,
+        # and in that case, there will be two description text.
+        # The code here can handle either single string or a list with strings,
+        # and it will normalize into a list with LangStringModel in both cases.
+        self.desc = MultipleLangStringModelList('desc')
+        if isinstance(data["desc"], str):
+            self.desc.append(i18n_translator[data["desc"]])
+        else:
+            for i in data["desc"]: self.desc.append(i18n_translator[i])
 
     def get_unique_id(self, is_joined: bool = True):
         if is_joined:
@@ -35,7 +45,7 @@ class StoryInfo(ToJsonReminderMixin):
             "file_type": self.file_type,
 
             "name": self.name.to_json()[-1],
-            "desc": self.desc.to_json()[-1],
+            "desc": [i[-1] for i in self.desc.to_json()[-1]],
             "pos": self.pos.to_json()[-1],
             "image": self.data["image"],
 
