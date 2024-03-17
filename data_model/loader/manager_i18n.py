f9937a5e44f4712995b170ca83153c403bc793f3
sctop <2094880085@qq.com>
2024-03-17 14:15:32 +0800

    i18n reduction to en/jp/zh_cn

--- data_model/loader/manager_i18n.py
+++ data_model/loader/manager_i18n.py
@@ -52,19 +52,43 @@ class TranslationManager:
 
     def __getitem__(self, item) -> LangStringModel:
         temp = dict((key, value.get(item)) for key, value in self.translations.items())
-        if temp["zh_cn"] == "":
-            # Special i18n case: Student Name, etc.
-            # There are at lease 3 variants when talking about that in Chinese.
-            # - zh_cn_cn indicates the data from the Blue Archive (China Server)
-            # - zh_cn_tw indicates the data from the Blue Archive (Global/Taiwan Server)
-            #   But with the content being converted from Traditional to Simplified Chinese
-            # - zh_cn_jp indicates the data from a third-party unofficial translation of
-            #   the original Japanese content (which is widely-accepted as well).
-            m = ZhLangStringModel()
-            m.load(temp)
-        else:
+
+        # NEW: ZhLangStringModel 自动降级到 LangStringModel
+        # 由于 ZhLangStringModel 在导出到JSON后太占存储空间，为了爱和正义决定根据特殊情况将其自动
+        # 降级到 LangStringModel 进行处置
+        if temp["zh_cn_cn"] == temp["zh_cn_jp"] == temp["zh_cn_tw"]:
+            # 如果三者全部都一样，那不就是单语言？
+            temp["zh_cn"] = temp["zh_cn_cn"]
             m = LangStringModel()
             m.load(temp)
+        else:
+            # 单独考察三种小语言，是不是有两个是空的
+            try:
+                if temp["zh_cn_cn"] == "" and temp["zh_cn_jp"] == "":
+                    temp["zh_cn"] = temp["zh_cn_tw"]
+                elif temp["zh_cn_cn"] == "" and temp["zh_cn_tw"] == "":
+                    temp["zh_cn"] = temp["zh_cn_jp"]
+                elif temp["zh_cn_jp"] == "" and temp["zh_cn_tw"] == "":
+                    temp["zh_cn"] = temp["zh_cn_cn"]
+                else:
+                    # 哦原来降级不了啊
+                    raise AssertionError
+            except AssertionError:
+                # 降级不了，老老实实.jpg
+                # Special i18n case: Student Name, etc.
+                # There are at lease 3 variants when talking about that in Chinese.
+                # - zh_cn_cn indicates the data from the Blue Archive (China Server)
+                # - zh_cn_tw indicates the data from the Blue Archive (Global/Taiwan Server)
+                #   But with the content being converted from Traditional to Simplified Chinese
+                # - zh_cn_jp indicates the data from a third-party unofficial translation of
+                #   the original Japanese content (which is widely-accepted as well).
+                m = ZhLangStringModel()
+                m.load(temp)
+            else:
+                # 能降级就嗯降
+                m = LangStringModel()
+                m.load(temp)
+
         return m
 
     def query(self, key):
